14aa2185652c0507e30dda00a0823229901e89d8
Qaz!@#wsx#@!edC1 <marcin.szymanski.poczta@gmail.com>
2019-07-17 14:34:16 +0200

    changed fiew lines

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -5,12 +5,12 @@ import com.kodilla.stream.lambda.*;
 public class StreamMain {
     public static void main(String[] args) {
         Processor process = new Processor();
-       /* ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
+        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
         SaySomething saySomething = new SaySomething();
         process.execute(executeSaySomething);
         process.execute(saySomething);
-        */
+
        //lambda
-        process.execute(()->System.out.println("Test lambda"));
+       // process.execute(()->System.out.println("Test lambda"));
     }
 }
