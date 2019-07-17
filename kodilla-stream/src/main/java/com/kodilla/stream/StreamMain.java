92162a692a5b6cc3272cf7de24aa3225b84fbca3
Qaz!@#wsx#@!edC1 <marcin.szymanski.poczta@gmail.com>
2019-07-17 14:14:08 +0200

    first commit

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,4 +1,16 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.lambda.*;
+
 public class StreamMain {
+    public static void main(String[] args) {
+        Processor process = new Processor();
+       /* ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
+        SaySomething saySomething = new SaySomething();
+        process.execute(executeSaySomething);
+        process.execute(saySomething);
+        */
+       //lambda
+        process.execute(()->System.out.println("Test lambda"));
+    }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/lambda/SaySomething.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/lambda/SaySomething.java
@@ -1,4 +1,9 @@
 package com.kodilla.stream.lambda;
 
-public class SaySomething {
+public class SaySomething implements
+Executor{
+    @Override
+    public void process() {
+        System.out.println("This is an example text1.");
+    }
 }
