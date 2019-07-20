053976c898e251c9845bf071cd3582b419e97ca4
Qaz!@#wsx#@!edC1 <marcin.szymanski.poczta@gmail.com>
2019-07-20 12:11:48 +0200

    re-uploaded

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -6,8 +6,13 @@ import java.time.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.reference.PoemBeautifierReference;
+import com.kodilla.stream.beautifier.reference.PoemExecutor;
+
 public class StreamMain {
     public static void main(String[] args) {
+        /*
         Forum forum = new Forum();
 
         Map<Integer, ForumUser>theMapUsers = forum.getForumUserList().stream()
@@ -19,5 +24,16 @@ public class StreamMain {
         theMapUsers.entrySet().stream()
                 .map(entry -> entry.getKey()+" : "+entry.getValue())
                 .forEach(System.out::println);
+
+         */
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+              poemBeautifier.beautify("tapped using small letters",(m)->m.toUpperCase());
+              poemBeautifier.beautify("TAPPED using CAPITAL LETTERS", (m)->m.toLowerCase());
+              poemBeautifier.beautify("Lorem ipsum dolor sit amet.",(m)->m.replace('m','Z'));
+
+                PoemExecutor poemExecutor = new PoemExecutor();
+                poemExecutor.executeBeauti("Lorem ipsum",PoemBeautifierReference::doSth);
+
     }
 }
