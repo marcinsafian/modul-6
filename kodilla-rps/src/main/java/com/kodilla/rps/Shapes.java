0ffea47bb2179170c7bc1a33df60e5c0cfb8bc1b
Qaz!@#wsx#@!edC1 <marcin.szymanski.poczta@gmail.com>
2019-07-24 16:27:32 +0200

    fix file

---
+++ kodilla-rps/src/main/java/com/kodilla/rps/Shapes.java
@@ -0,0 +1,15 @@
+package com.kodilla.rps;
+
+public enum  Shapes {
+    STONE("stone"),PAPER("paper"),SCISSORS("scissors");
+
+    private final String text;
+
+    Shapes(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
