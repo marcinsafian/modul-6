66f9b43f0eef979e1ced2c5601d4b1260341d4d2
Marcin Szymanski <marcin.szymanski.poczta@gmail.com>
2019-07-29 14:19:09 +0200

    added Exceptions

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearcher.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearcher.java
@@ -1,5 +1,32 @@
 package com.kodilla.exception.test;
 
-public class FlightSearcher {
+import java.util.HashMap;
+import java.util.Map;
 
+public class FlightSearcher {
+private final Map<String,Boolean>airportMap;
+        public FlightSearcher(){
+    airportMap = airportBase();
+        }
+        public boolean findFlight(Flight flight) throws RouteNotFoundException{
+            return findAirport(flight.getArrivalAirport())&&findAirport(flight.getDepartureAirport());
+        }
+       private boolean findAirport(String airportName) throws RouteNotFoundException{
+            if(airportMap.containsKey(airportName)){
+                return airportMap.get(airportName);
+            }
+            else {
+                throw new RouteNotFoundException("AirPort does not exist");
+            }
+        }
+        private Map<String,Boolean>airportBase(){
+            Map<String,Boolean>airportBase = new HashMap<>();
+            airportBase.put("WIEN",false);
+            airportBase.put("WAR",true);
+            airportBase.put("MOSC",false);
+            airportBase.put("PRAG",true);
+            airportBase.put("NOWHERE",false);
+
+            return airportBase;
+        }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/RunFlight.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/RunFlight.java
@@ -2,6 +2,37 @@ package com.kodilla.exception.test;
 
 public class RunFlight {
     public static void main(String[] args) {
+        Flight wienWar = new Flight("WIEN","War");
+        Flight moscPrag = new Flight("MOSC","PRAG");
+        Flight warPrag = new Flight("WAR","PRAG");
+        Flight noName = new Flight("NOWHERE","NAME");
 
+        FlightSearcher flightSearcher = new FlightSearcher();
+
+        try {
+            boolean existWienWar = flightSearcher.findFlight(wienWar);
+            System.out.println("Flight Vienna Warsaw: "+existWienWar);
+        }catch (RouteNotFoundException e){
+            System.out.println("You are not allowed to fly");
+        }
+        try {
+            boolean existMoscPrag = flightSearcher.findFlight(moscPrag);
+            System.out.println("Flight Moscow Prague: "+existMoscPrag);
+        }catch (RouteNotFoundException e){
+            System.out.println("You are not allowed to fly");
+        }
+
+        try {
+            boolean existWarPrag = flightSearcher.findFlight(warPrag);
+            System.out.println("Flight Warsaw Prague: "+existWarPrag);
+        }catch (RouteNotFoundException e){
+            System.out.println("You are not allowed to fly");
+        }
+        try {
+            boolean existNoName = flightSearcher.findFlight(noName);
+            System.out.println("Flight from Nowhere to Nowhere: "+existNoName);
+        }catch (RouteNotFoundException e){
+            System.out.println("You are not allowed to fly");
+        }
     }
 }
