8d26b16c98ca3074500c9d09c54d88035d28edb8
Marcin Szymanski <marcin.szymanski.poczta@gmail.com>
2019-08-30 09:04:05 +0200

    final iteration

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/App.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/App.java
@@ -1,5 +1,6 @@
 package com.kodilla.good.patterns.challenges;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -13,5 +14,21 @@ public class App {
                 .stream()
                 .forEach(entry -> System.out.println(entry.getKey()+ " "+entry.getValue().stream().collect(Collectors.joining(" ! "))));
 
+        final String movies = movieMap.entrySet()
+                .stream()
+                .flatMap(entrry -> entrry.getValue().stream())
+                .collect(Collectors.joining(" ! "));
+        System.out.println(movies);
+
+        final String abbreviation = movieMap.entrySet()
+                .stream()
+                .flatMap(n -> {
+                    List<String> list = new ArrayList<>();
+                    list.add(n.getKey());
+                    list.addAll(n.getValue());
+                    return list.stream();
+                })
+                .collect(Collectors.joining(" ! "));
+        System.out.println(abbreviation);
     }
 }
