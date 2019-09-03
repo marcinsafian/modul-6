dd57cfc12398ff9d218fe7e36e8c00cbce6bc0cc
Marcin Szymanski <marcin.szymanski.poczta@gmail.com>
2019-09-03 17:42:31 +0200

    initial commit -modul 13.4

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2Door/GlutenFreeProduct.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2Door/GlutenFreeProduct.java
@@ -1,4 +1,28 @@
 package com.kodilla.good.patterns.food2Door;
 
-public class GlutenFreeProduct {
+public class GlutenFreeProduct extends Product {
+    private String productDescription;
+
+    public GlutenFreeProduct(String productName, String productDescription) {
+        super(productName);
+        this.productDescription = productDescription;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof GlutenFreeProduct)) return false;
+        if (!super.equals(o)) return false;
+
+        GlutenFreeProduct that = (GlutenFreeProduct) o;
+
+        return productDescription.equals(that.productDescription);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + productDescription.hashCode();
+        return result;
+    }
 }
