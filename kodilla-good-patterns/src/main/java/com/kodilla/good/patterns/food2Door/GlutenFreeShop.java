5aee6206295d6d1fe326c0d29e3ad7d8af329318
Marcin Szymanski <marcin.szymanski.poczta@gmail.com>
2019-09-04 12:57:23 +0200

    initial commit -modul 13.4

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2Door/GlutenFreeShop.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2Door/GlutenFreeShop.java
@@ -1,4 +1,44 @@
 package com.kodilla.good.patterns.food2Door;
 
-public class GlutenFreeShop {
+import java.util.*;
+
+public class GlutenFreeShop implements FoodProducer {
+    private Map<Product,Integer> productList;
+    private List poisonList;
+
+    public GlutenFreeShop(){
+        productList = createProductList();
+        poisonList = createPoisonList();
+    }
+    private Map<Product, Integer> createProductList(){
+        Map<Product, Integer> productList = new HashMap<>();
+        productList.put(new GlutenFreeProduct("bread", "Bread made from coconut flour"), 50);
+        productList.put(new GlutenFreeProduct("rice", "extraordinary white rice"), 150);
+        productList.put(new GlutenFreeProduct("cookies", "amaranth cookies with dactyls"), 20);
+        productList.put(new GlutenFreeProduct("coconut flour", "finely ground coconut flour"), 4);
+
+        return productList;
+    }
+    private List<Customer> createPoisonList(){
+        List<Customer> poisonList = new ArrayList<>();
+        poisonList.add(new Customer("Micky Mouse"));
+
+        return poisonList;
+    }
+
+    @Override
+    public boolean process(Customer customer, Map<Product, Integer> productsOrders) {
+        return (!poisonList.contains(customer) && isProductsAvailable(productsOrders));
+    }
+
+    private boolean isProductsAvailable(Map<Product, Integer> productOrders) {
+        for (Map.Entry<Product, Integer> entry : productOrders.entrySet()) {
+            Optional<Integer> productQty = Optional.ofNullable(productList.get(entry.getKey()));
+            if (productQty.orElse(0) < entry.getValue()) {
+                System.out.println("Gluten Free Shop: We're sorry. Product is unavailable.");
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2Door/HealthyShop.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2Door/HealthyShop.java
@@ -1,4 +1,37 @@
 package com.kodilla.good.patterns.food2Door;
 
-public class HealthyShop {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class HealthyShop implements FoodProducer {
+    private Map<Product,Integer> productList;
+
+    public HealthyShop(){
+        productList = createProductList();
+    }
+    private Map<Product,Integer> createProductList(){
+        Map<Product,Integer> productList =new HashMap<>();
+        productList.put(new GlutenFreeProduct("pasta", "Noodles made from rice flour"), 20);
+        productList.put(new GlutenFreeProduct("corn crisps", "Crisps made from corn flour"), 15);
+        productList.put(new GrainProduct("spaghetti pasta", true), 20);
+        productList.put(new GrainProduct("spelt flour", true), 4);
+
+        return productList;
+    }
+    @Override
+    public boolean process(Customer customer, Map<Product, Integer> productsOrders) {
+        return (productsOrders.size() >= 2 && isProductsAvailable(productsOrders));
+    }
+
+    private boolean isProductsAvailable(Map<Product, Integer> productsOrders) {
+        for (Map.Entry<Product, Integer> entry : productsOrders.entrySet()) {
+            Optional<Integer> productQty = Optional.ofNullable(productList.get(entry.getKey()));
+            if (productQty.orElse(0) < entry.getValue()) {
+                System.out.println("Healthy Shop: We're sorry. Product is unavailable.");
+                return false;
+            }
+        }
+        return true;
+    }
 }
