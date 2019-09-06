55ee479a7d5d7e49834c3daf313b80747eb2910b
Marcin Szymanski <marcin.szymanski.poczta@gmail.com>
2019-09-06 09:14:28 +0200

    added TestSuite -modul 14.1

--- kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuit.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuit.java
@@ -1,4 +1,24 @@
 package com.kodilla.spring.forum;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ForumUserTestSuit {
+    @Test
+    public void testUserName(){
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        ForumUser forumUser = context.getBean(ForumUser.class);
+        //When
+        String user = forumUser.getUserName();
+        //Then
+        Assert.assertEquals("John Smith",user);
+    }
 }
