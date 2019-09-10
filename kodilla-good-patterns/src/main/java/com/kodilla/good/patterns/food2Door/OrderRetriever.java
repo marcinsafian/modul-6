c8e80f1a705321eb0f7235bed5b0eb144c7c3b4c
Marcin Szymanski <marcin.szymanski.poczta@gmail.com>
2019-09-10 20:42:42 +0200

    nothing sent

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2Door/OrderRetriever.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2Door/OrderRetriever.java
@@ -11,24 +11,24 @@ public class OrderRetriever {
      List<OrderRequest> orderRequestList = new ArrayList<>();
 
      Customer customer = new Customer("Jaś Fasola");
-     FoodProducer foodProducer = new GlutenFreeShop();
+     FoodProducer foodProducer = new HealthyShop();
+        sth(orderRequestList, customer, foodProducer);
+
+        Customer customer1 = new Customer("Mickey");
+        FoodProducer foodProducer1 = new GlutenFreeShop();
+        sth(orderRequestList, customer1, foodProducer1);
+
+
+        return orderRequestList;
+    }
+
+    private void sth(List<OrderRequest> orderRequestList, Customer customer, FoodProducer foodProducer) {
         Map<Product, Integer> productList = new HashMap<>();
 
         productList.put(new GlutenFreeProduct("bread", "Bread made from coconut flour"), 2);
         productList.put(new GlutenFreeProduct("rice", "extraordinary white rice"), 3);
-        productList.put(new GlutenFreeProduct("cookies", "amaranth cookies with dactyls"), 1000);
-
-        orderRequestList.add(new OrderRequest(customer,foodProducer,productList));
-
-        Customer customer1 = new Customer("Mickey");
-        FoodProducer foodProducer1 = new GlutenFreeShop();
-        Map<Product, Integer> productsList1 = new HashMap<>();
-        productList.put(new GlutenFreeProduct("bread", "Bread made from coconut flour"), 2);
-        productList.put(new GlutenFreeProduct("rice", "extraordinary white rice"), 3);
         productList.put(new GlutenFreeProduct("cookies", "amaranth cookies with dactyls"), 1);
 
-        orderRequestList.add(new OrderRequest(customer1, foodProducer1, productsList1));
-
-        return orderRequestList;
+        orderRequestList.add(new OrderRequest(customer,foodProducer,productList));
     }
 }
