78304bb90c42fc5621df563e28c072840a073d07
Marcin Szymanski <marcin.szymanski.poczta@gmail.com>
2019-09-10 10:49:36 +0200

    initial commit

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/TaskList.java
@@ -0,0 +1,26 @@
+package com.kodilla.spring.portfolio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    List<String> tasks;
+
+    public TaskList(){
+        tasks = new ArrayList<>();
+    }
+    public boolean addTask(String taskDescription){
+        return tasks.add(taskDescription);
+    }
+
+    @Override
+    public String toString() {
+        return "TaskList{" +
+                "tasks=" + tasks +
+                '}';
+    }
+
+    public List<String> getTasks() {
+        return tasks;
+    }
+}
