d06e7c006c90cafd5643e0464939e893eb6aa73e
Marcin Szymanski <marcin.szymanski.poczta@gmail.com>
2019-09-11 18:30:24 +0200

    test suite -modul 15.1

--- kodilla-patterns/src/main/java/com/kodilla/patterns/singleton/Logger.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/singleton/Logger.java
@@ -2,6 +2,19 @@ package com.kodilla.patterns.singleton;
 
 public class Logger {
     private String lastLog = "";
+    private static Logger logger = null;
+
+    private Logger(){}
+    public static Logger getInstance(){
+        if (logger == null){
+            synchronized (Logger.class){
+                if (logger == null){
+                    logger = new Logger();
+                }
+            }
+        }
+        return logger;
+    }
 
     public void log(String log) {
         lastLog = log;
