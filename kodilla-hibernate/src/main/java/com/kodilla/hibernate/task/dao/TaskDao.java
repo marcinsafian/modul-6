f3b229dbc278cf3bb19934760db5c14c8d0ec98b
Marcin Szymanski <marcin.szymanski.poczta@gmail.com>
2019-09-26 15:39:22 +0200

    fix commit

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface TaskDao extends CrudRepository<Task, Integer> {
         List<Task> findByDuration(int duration);
+
 }
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
@@ -48,7 +48,7 @@ public class TaskDaoTestSuite {
         Assert.assertEquals(1, readTasks.size());
 
         //CleanUp
-        //int id = readTasks.get(0).getId();
-        //taskDao.deleteById(id);
+        int id = readTasks.get(0).getId();
+        taskDao.deleteById(id);
     }
 }
