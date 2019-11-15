1256f9f46d71357c39cc83f2e62518cfa04cc176
Marcin Szymanski <marcin.szymanski.poczta@gmail.com>
2019-11-15 21:56:18 +0100

    update

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/facade/ShopServiceTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/facade/ShopServiceTestSuite.java
@@ -4,6 +4,7 @@ import com.kodilla.patterns2.facade.api.ItemDto;
 import com.kodilla.patterns2.facade.api.OrderDto;
 import com.kodilla.patterns2.facade.api.OrderFacade;
 import com.kodilla.patterns2.facade.api.OrderProcessingException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
