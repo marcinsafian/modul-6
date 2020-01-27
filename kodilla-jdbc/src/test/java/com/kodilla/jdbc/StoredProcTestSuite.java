7060a264b06a566c614dd360581f71cd768d3267
MSss <marcin.szymanski.poczta@gmail.com>
2020-01-27 19:54:21 +0100

    add sqlTest - UpdateBestsellers

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -32,4 +32,25 @@ public class StoredProcTestSuite {
         }
         assertEquals(0,howMany);
     }
+    @Test
+    public void testUpdateBestsellers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE books SET bestseller=null";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+
+        //When
+        String sqlProcedureCall = "CALL UpdateBestsellers()";
+        statement.execute(sqlProcedureCall);
+
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) AS how_many FROM books WHERE bestseller is null";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if (rs.next()) {
+            howMany = rs.getInt("how_many");
+        }
+        assertEquals(4, howMany);
+    }
 }
